d9498e6dab914a28c808132610b69f9cd4c4e973
Đăng Khoa <58596136+khoatvbattu@users.noreply.github.com>
2022-08-14 11:47:02 +0700

    ReadFile

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.*;
+import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,78 +10,171 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args){
+public final class Main {
 
-        ReadFile_Array();
+    /***
+     * User scanner from keyboard.
+     */
+    private static Scanner input;
+
+    /***
+     * User choice.
+     */
+    private static int choice;
+
+    /**
+     * Constructor.
+     */
+    private Main() {
+
     }
 
-    private static void ReadFile_String()
-    {
-        String filePath = "src/main/java/Data/Subjects.txt";
-        // Declaring object of StringBuilder class
-        StringBuilder builder = new StringBuilder();
+    public static void main(final String[] args) {
 
-        // try block to check for exceptions where
-        // object of BufferedReader class us created
-        // to read filepath
-        try (BufferedReader buffer = new BufferedReader(
-                new FileReader(filePath))) {
+        choice = 0;
+        input = new Scanner(System.in);
 
-            String str;
+        StudentList.init();
 
-            // Condition check via buffer.readLine() method
-            // holding true upto that the while loop runs
-            while ((str = buffer.readLine()) != null) {
+        Boolean isExit = false;
+        String temp = "";
 
-                builder.append(str).append("\n");
+        while (true) {
+            System.out.println("Student Management System");
+            System.out.println("#" + Constants.ADD_STUDENT_FUNCTION + ". Add student");
+            System.out.println("#" + Constants.UPDATE_STUDENT_FUNCTION + ". Update student info");
+            System.out.println("#" + Constants.DETELE_STUDENT_FUNCTION + ". Delete sudent");
+            System.out.println("#" + Constants.SHOW_STUDENT_FUNCTION + ". Show student list");
+            System.out.println("#" + Constants.SORT_STUDENT_FUNCTION + ". Sort student list");
+            System.out.println("#" + Constants.IMPORT_STUDENT_FUNCTION + ". Import student list");
+            System.out.println("#" + Constants.EXPORT_STUDENT_FUNCTION + ". Export student list");
+            System.out.println("#" + Constants.EXIT_FUNCTION + ". Exit");
+
+            // get user choice
+            while (true) {
+                System.out.print("(?) Your choice: ");
+                temp = input.nextLine();
+                if (InputChecker.isInteger(temp)) {
+                    choice = Integer.parseInt(temp);
+                    break;
+                } else {
+                    System.out.println("(!) Invalid scanner data");
+                }
+            }
+
+            // solve
+            switch (choice) {
+                case Constants.ADD_STUDENT_FUNCTION:
+                    StudentList.addList();
+                    break;
+                case Constants.UPDATE_STUDENT_FUNCTION:
+                    StudentList.editList();
+                    break;
+                case Constants.DETELE_STUDENT_FUNCTION:
+                    StudentList.deleteList();
+                    break;
+                case Constants.SHOW_STUDENT_FUNCTION:
+                    StudentList.showList();
+                    break;
+                case Constants.SORT_STUDENT_FUNCTION:
+                    StudentList.sortList();
+                    break;
+                case Constants.IMPORT_STUDENT_FUNCTION:
+                    // get file type info
+                    int choiceFileTypeIM;
+
+                    System.out.println("Choose file type");
+                    System.out.println("#1. File binary");
+                    System.out.println("#2. File text (csv)");
+
+                    // get right input from user
+                    while (true) {
+                        System.out.print("(?) Your choice: ");
+                        temp = input.nextLine();
+
+                        if (InputChecker.isInteger(temp)) {
+                            choiceFileTypeIM = Integer.parseInt(temp);
+                            if (choiceFileTypeIM == 1) {
+                                StudentList.importList("bin");
+                            } else if (choiceFileTypeIM == 2) {
+                                StudentList.importList("text");
+                            } else {
+                                System.out.println("(!) Invalid choice");
+                            }
+                            break;
+                        } else {
+                            System.out.println("(!) Invalid scanner data");
+                        }
+                    }
+                    break;
+
+                case Constants.EXPORT_STUDENT_FUNCTION:
+                    // get file type info
+                    int choiceFileTypeEX;
+                    System.out.println("Choose file type?");
+                    System.out.println("#1. File binary");
+                    System.out.println("#2. File text (csv)");
+
+                    // get right input from user
+                    while (true) {
+                        System.out.print("(?) Your choice: ");
+                        temp = input.nextLine();
+
+                        if (InputChecker.isInteger(temp)) {
+                            choiceFileTypeEX = Integer.parseInt(temp);
+                            if (choiceFileTypeEX == 1) {
+                                StudentList.exportList("bin");
+                            } else if (choiceFileTypeEX == 2) {
+                                StudentList.exportList("text");
+                            } else {
+                                System.out.println("(!) Invalid choice");
+                            }
+                            break;
+                        } else {
+                            System.out.println("(!) Invalid scanner data");
+                        }
+                    }
+                    break;
+                case Constants.EXIT_FUNCTION:
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+
+            //
+            if (isExit) {
+                break;
+            }
+
+            //
+            clearConsole();
+            System.out.println("Press enter to continue");
+
+            // pause console
+            while (temp != null) {
+                if (temp.isEmpty()) {
+                    break;
+                }
+
+                if (input.hasNextLine()) {
+                    temp = input.nextLine();
+                } else {
+                    temp = null;
+                }
             }
         }
 
-        // Catch block to handle the exceptions
-        catch (IOException e) {
-
-            // Print the line number here exception occurred
-            // using printStackTrace() method
-            e.printStackTrace();
-        }
-
-        // Returning a string
-        System.out.println(builder.toString());
+        clearConsole();
+        System.out.println("---Thank you for using my program---");
     }
 
-    public static void ReadFile_Array(){
-        try{
-            // list that holds strings of a file
-            List<String> listOfStrings
-                    = new ArrayList<String>();
-
-            // load data from file
-            BufferedReader bf = new BufferedReader(
-                    new FileReader("src/main/java/Data/Subjects.txt"));
-
-            // read entire line as string
-            String line = bf.readLine();
-
-            // checking for end of file
-            while (line != null) {
-                listOfStrings.add(line);
-                line = bf.readLine();
-            }
-
-            // closing bufferreader object
-            bf.close();
-
-            // storing the data in arraylist to array
-            String[] array
-                    = listOfStrings.toArray(new String[0]);
-
-            // printing each line of file
-            // which is stored in array
-            for (String str : array) {
-                System.out.println(str);
-            }
-        } catch (IOException e){}
-
+    /***
+     * Use to clear console.
+     * Removed code because eclipse's console error.
+     */
+    private static void clearConsole() {
+        System.out.println("-------------------------------------------------");
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,49 +1,204 @@
 package org.example;
 
-public class Student {
-    public String getId() {
-        return Id;
+
+public final class Student {
+
+    /***
+     * Student id.
+     */
+    private int id;
+
+    /***
+     * Student name.
+     */
+    private String name;
+
+    /***
+     * Student score.
+     */
+    private float score;
+
+    /***
+     * Student image.
+     */
+    private String image;
+
+    /***
+     * Student address.
+     */
+    private String address;
+
+    /***
+     * Student description.
+     */
+    private String description;
+
+    /**
+     * @return The id
+     */
+    public int getID() {
+        return id;
     }
 
-    public void setId(String id) {
-        Id = id;
+    /**
+     * @param idInput
+     *            The id to set.
+     */
+    public void setID(final int idInput) {
+        this.id = idInput;
     }
 
-    public String getFullName() {
-        return FullName;
+    /**
+     * @return The name
+     */
+    public String getName() {
+        return name;
     }
 
-    public void setFullName(String fullName) {
-        FullName = fullName;
+    /**
+     * @param nameInput
+     *            The name to set.
+     */
+    public void setName(final String nameInput) {
+        this.name = nameInput;
     }
 
-    public int getPhone() {
-        return Phone;
+    /**
+     * @return The score
+     */
+    public float getScore() {
+        return score;
     }
 
-    public void setPhone(int phone) {
-        Phone = phone;
+    /**
+     * @param scoreInput
+     *            The score to set.
+     */
+    public void setScore(final float scoreInput) {
+        this.score = scoreInput;
     }
 
+    /**
+     * @return The image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param imageInput
+     *            The image to set.
+     */
+    public void setImage(final String imageInput) {
+        this.image = imageInput;
+    }
+
+    /**
+     * @return The address
+     */
     public String getAddress() {
-        return Address;
+        return address;
     }
 
-    public void setAddress(String address) {
-        Address = address;
+    /**
+     * @param addressInput
+     *            The address to set.
+     */
+    public void setAddress(final String addressInput) {
+        this.address = addressInput;
     }
 
-    public String getSpecialized() {
-        return Specialized;
+    /**
+     * @return The description
+     */
+    public String getDescription() {
+        return description;
     }
 
-    public void setSpecialized(String specialized) {
-        Specialized = specialized;
+    /**
+     * @param descriptionInput
+     *            The description to set.
+     */
+    public void setDescription(final String descriptionInput) {
+        this.description = descriptionInput;
     }
 
-    private String Id;
-    private String FullName;
-    private int Phone;
-    private String Address;
-    private String Specialized;
+    /**
+     * @param idInput
+     *            The id to set.
+     * @param nameInput
+     *            The name to set.
+     * @param scoreInput
+     *            The score to set.
+     * @param imageInput
+     *            The image to set.
+     * @param addressInput
+     *            The address to set.
+     * @param descriptionInput
+     *            The description to set.
+     */
+    public Student(final int idInput, final String nameInput,
+                   final float scoreInput, final String imageInput,
+                   final String addressInput, final String descriptionInput) {
+        super();
+        this.id = idInput;
+        this.name = nameInput;
+        this.score = scoreInput;
+        this.image = imageInput;
+        this.address = addressInput;
+        this.description = descriptionInput;
+
+    }
+
+    /**
+     *
+     */
+    public Student() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("     Student ID         :");
+        sb.append(id);
+        sb.append("\n");
+        sb.append("     Student Name       :");
+        sb.append(name);
+        sb.append("\n");
+        sb.append("     Student Score      :");
+        sb.append(score);
+        sb.append("\n");
+        sb.append("     Student Image      :");
+        sb.append(image);
+        sb.append("\n");
+        sb.append("     Student Address    :");
+        sb.append(address);
+        sb.append("\n");
+        sb.append("     Student Description:");
+        sb.append(description);
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    /***
+     * to string to save to file.
+     *
+     * @return string
+     */
+    public String toStringSaveFile() {
+
+        String result = id + "," + name + "," + score + "," + image + ","
+                + address + "," + description;
+
+        return result;
+    }
+
 }
